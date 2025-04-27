c7ffa9f3faeb2c5ec09efc79e76778a0fa4fba1b
Tina Nguyen <134570922+tinabnguyen@users.noreply.github.com>
2025-04-27 13:29:12 -0500

    Perceptron model

--- final_project.py
+++ final_project.py
@@ -1,17 +1,19 @@
 import pandas as pd
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# Load the dataset
-df = pd.read_csv("merged_dataset.csv")  # Replace with your actual filename
+# ----------------------- Splitting Kaggle dataset into train and test sets -----------------------
+df = pd.read_csv("merged_dataset.csv")
 
-# Drop Transaction.Date column
+# we don't need the 'Transaction.Date' column for our analysis, so we removed it.
 df.drop(columns=['Transaction.Date'], inplace=True)
 
-# Encode categorical columns
 categorical_columns = ['source', 'browser', 'sex', 'Payment.Method',
                        'Product.Category', 'Device.Used']
 
+# Convert categorical columns to numerical using Label Encoding for easier feature analysis.
 le = LabelEncoder()
 for col in categorical_columns:
     df[col] = le.fit_transform(df[col])
@@ -29,3 +31,18 @@ X_train.to_csv("ecommerceX_train.csv", index=False)
 X_test.to_csv("ecommerceX_test.csv", index=False)
 y_train.to_csv("ecommerceY_train.csv", index=False)
 y_test.to_csv("ecommerceY_test.csv", index=False)
+
+
+# ----------------------- Training the Perceptron model -----------------------
+def train_perceptron(X_train, y_train):
+    # Initialize the Perceptron model
+    perceptron = Perceptron(max_iter=1000, tol=1e-3, random_state=42)
+
+    # Fit the model to the training data
+    perceptron.fit(X_train, y_train)
+    y_pred = perceptron.predict(X_train)
+    sklearn_cm = confusion_matrix(y_train, y_pred)
+    print("Confusion Matrix:\n", sklearn_cm)
+
+
+train_perceptron(X_train, y_train)
